c01916f46bca07e5fc4b906282384e51f429acdd
Anderson, Christopher J <Chris.Anderson@capitalone.com>
2016-08-27 12:47:39 -0400

    Updates to Savings Goal Activity
    Remove unused files

--- app/src/main/java/com/androidsummit/androidsummitsampleapp/balance/SavingsGoalActivity.java
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/balance/SavingsGoalActivity.java
@@ -1,5 +1,6 @@
 package com.androidsummit.androidsummitsampleapp.balance;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,28 +8,22 @@ import android.support.v7.widget.Toolbar;
 
 import com.androidsummit.androidsummitsampleapp.R;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+public class SavingsGoalActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
 
+    static Intent createLaunchIntent(Context context) {
+        Intent intent = new Intent(context, SavingsGoalActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_savings_goal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ButterKnife.bind(this);
-
-    }
-
-    @OnClick(R.id.main_purchase_goal_container)
-    void onPurchaseGoalClicked() {
-        Intent purchaseGoalActivity = SavingsGoalActivity.createLaunchIntent(this);
-        startActivity(purchaseGoalActivity);
+        toolbar.setNavigationIcon(R.drawable.ic_close);
     }
 
 }
