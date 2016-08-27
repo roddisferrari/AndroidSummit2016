1b3f84ba754e30bcdb550a2bbcf89770fab20e9b
Anderson, Christopher J <Chris.Anderson@capitalone.com>
2016-08-27 10:49:46 -0400

    Initial port of Sample App

---
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/balance/MainActivity.java
@@ -0,0 +1,17 @@
+package com.androidsummit.androidsummitsampleapp.balance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidsummit.androidsummitsampleapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
