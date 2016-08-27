8d5f7890e8926f8525413204cfc71d2aff0e7071
Anderson, Christopher J <Chris.Anderson@capitalone.com>
2016-08-27 13:05:34 -0400

    Add save goal functionality

--- app/src/main/java/com/androidsummit/androidsummitsampleapp/balance/SavingsGoalActivity.java
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/balance/SavingsGoalActivity.java
@@ -5,8 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import com.androidsummit.androidsummitsampleapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SavingsGoalActivity extends AppCompatActivity {
 
@@ -16,14 +23,54 @@ public class SavingsGoalActivity extends AppCompatActivity {
         return intent;
     }
 
+    @BindView(R.id.savings_goal_amount)
+    EditText goalAmount;
+
+    @BindView(R.id.savings_goal_description)
+    EditText goalDescription;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private TextWatcher textChangeListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            onEditTextChanged();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savings_goal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
 
         toolbar.setNavigationIcon(R.drawable.ic_close);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        goalAmount.addTextChangedListener(textChangeListener);
+        goalDescription.addTextChangedListener(textChangeListener);
+    }
+
+    private void onEditTextChanged() {
+
+        boolean hasDescription = !TextUtils.isEmpty(goalDescription.getText());
+        boolean hasAmount = !TextUtils.isEmpty(goalAmount.getText());
+
+        toolbar.setNavigationIcon(hasDescription && hasAmount ? R.drawable.ic_done : R.drawable.ic_close);
+
     }
 
 }
